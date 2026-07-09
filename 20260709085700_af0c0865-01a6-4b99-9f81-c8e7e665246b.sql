6aaf8385fe5987e3cdad11b1afe2f7c8097f3343
mostafahegab83-cpu <mostafa.hegab83@gmail.com>
2026-07-09 12:11:48 +0300

    Delete 20260709085700_af0c0865-01a6-4b99-9f81-c8e7e665246b.sql

--- 20260709085700_af0c0865-01a6-4b99-9f81-c8e7e665246b.sql
+++
@@ -1,6 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/gap-analysis/")({
-  beforeLoad: () => { throw redirect({ to: "/gap-analysis/assessment" }); },
-  component: () => null,
-});
